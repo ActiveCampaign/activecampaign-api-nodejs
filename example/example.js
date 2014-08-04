0be9e1e1c6f24cc3bb9c200e584ee93970c024c7
Matt Thommes <matt@activecampaign.com>
2014-08-04 14:52:19 -0500

    updated examples

--- example/example.js
+++ example/example.js
@@ -2,10 +2,17 @@ var ActiveCampaign = require("activecampaign");
 
 var ac = new ActiveCampaign("", "");
 ac.debug = true;
-//ac.version(2);
 
-/*ac.track_actid = "";
+// update general settings.
+var post_test = ac.api("settings/edit", {dateformat: "%m/%d/%Y", timeformat: "%H:%M", datetimeformat: "%m/%d/%Y %H:%M"}, function(response) {console.log(response);});
+
+ac.version(2);
+ac.track_actid = "";
 ac.track_key = "";
-ac.track_email = "test@test.com";*/
+ac.track_email = "test@test.com";
 
-var test = ac.api("settings/edit", {dateformat: "%m/%d/%Y", timeformat: "%H:%M", datetimeformat: "%m/%d/%Y %H:%M"}, function(response) {console.log(response);});
+// return whitelisted domains for site tracking.
+var tracking_test1 = ac.api("tracking/site/list", {}, function(response) {console.log(response);});
+
+// return saved events.
+var tracking_test2 = ac.api("tracking/event/list", {}, function(response) {console.log(response);});
