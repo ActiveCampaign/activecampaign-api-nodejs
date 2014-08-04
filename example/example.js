5417394c9cd780bb11c4100a7ff6700c138e2e7d
Matt Thommes <matt@activecampaign.com>
2014-08-04 14:45:39 -0500

    added Settings object, and updated example file

--- example/example.js
+++ example/example.js
@@ -2,47 +2,10 @@ var ActiveCampaign = require("activecampaign");
 
 var ac = new ActiveCampaign("", "");
 ac.debug = true;
+//ac.version(2);
 
-//console.log(ac);
+/*ac.track_actid = "";
+ac.track_key = "";
+ac.track_email = "test@test.com";*/
 
-var contact_exists = ac.api("contact/view?email=test@test.com", {}, function(response) {
-console.log(response);
-/*
-  var contact;
-  if(response){
-    if(response.lists[30]){     //If subscriner is already subscribed, does nothing
-      return;
-    }
-    contact = {
-      email: response.email,
-      'p[123]': 30,                                       //adds subscriber to list
-      'status[123]': 1,
-    };
-    if(response.first_name){  
-      contact.first_name = response.first_name;
-    }
-    if(response.last_name){
-      contact.last_name = response.last_name;
-    }
-    if(response.fields[4]){
-      contact.fields[28, 0] = response.fields[4].val;     //adds field name if available
-    }
-
-    var contact_delete = ac.api('contact/delete', {'id': response.id}, function(nestedresponse){
-      console.log(nestedresponse);
-    });
-  }
-
-// If contact is not in the list, the following blocks of code should still execute, but they don't  
-
-  contact.email = "test@example.org";
-  contact.p[123] = 30;
-  contact.status[123] = 1;
-
-  // Add the contact to the subscribed list
-
-  var contact_add = ac.api("contact/add", contact, function(response){
-    console.log(response);
-  });
-*/
-});
+var test = ac.api("settings/edit", {dateformat: "%m/%d/%Y", timeformat: "%H:%M", datetimeformat: "%m/%d/%Y %H:%M"}, function(response) {console.log(response);});
