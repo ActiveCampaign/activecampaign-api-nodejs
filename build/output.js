a11fa41ee093eaee1af23f1f9857f97d1137667e
Joshua Bartlett <joshua.r.bartlett@gmail.com>
2018-03-12 21:43:34 -0500

    Improve build system

--- build/output.js
+++ build/output.js
@@ -1,15 +1,6 @@
-const resolve = require('rollup-plugin-node-resolve');
-const babel = require('rollup-plugin-babel');
-
 module.exports = {
   file: "dist/index.js",
   format: "umd",
   name: "ActiveCampaign",
-  strict: false,
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**' // only transpile our source code
-    })
-  ]
+  strict: false
 }
